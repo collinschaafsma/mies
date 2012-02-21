6af39ae63a8b33a9d35b02a4a55666f401e15312
Collin Schaafsma <collin@quickleft.com>
2012-02-20 21:00:58 -0700

    initial rails new with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mies::Application.config.secret_token = '83be820813efcdfc6033b8565205df416cd89f4a2880796c13c9530a6ea4f65b6f44c621debcf43f6809a05258e0c3f46cec61e1f2de596c7fa750cdc69f988f'
